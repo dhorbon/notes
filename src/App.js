9b9de1604e98630e8d8ec049b8ae625e13d24015
dhorbon <szymon080206@gmail.com>
2024-02-24 12:43:42 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [input, setInput] = useState("")
+  const [notes, setNotes] = useState(() => {
+    const local = localStorage.getItem("ITEMS")
+    if(local == null) return []
+    return JSON.parse(local)
+  })
+  const [id, setId] = useState(0);
 
-export default App;
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(notes))
+  }, [notes])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setNotes(howDoesThisWork => {
+      return [...howDoesThisWork, {id: id, note: input, checked: false}]
+    })
+    setId(id+1)
+
+    setInput("")
+  }
+
+  function clickCheck(id, checked){
+    setNotes(whyDoesThisWork => {
+      return whyDoesThisWork.map(note => {
+        if(note.id === id){
+          return {...note, checked} 
+        }
+
+        return note
+      })
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes(iDontWantToDoItLikeThat => {
+      return iDontWantToDoItLikeThat.filter(note => note.id !== id)
+    })
+  } 
+
+  return <>
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input value={input} onChange={e => setInput(e.target.value)} type="text" id="item" />
+      </div>
+      <button className="btn">Add a note</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      {notes.map(note =>{
+        return(
+          <li key={note.id}>
+            <label>
+              <input type="checkbox" checked={note.checked} onChange={e=>clickCheck(note.id, e.target.checked)} />
+              {note.note}
+            </label>
+            <button onClick={() => deleteNote(note.id)} className="btn btn-danger">Delete</button>
+          </li>
+        )
+      })}
+    </ul>
+  </>
+};
